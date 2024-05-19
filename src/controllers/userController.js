fc9686d1dbee254769a6032eed21c67997d8bfe8
Le Tien <tienle21032001@gmail.com>
2024-05-19 20:34:59 +0700

    get event followers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,10 @@
+
 /** @format */
 
 const asyncHandle = require('express-async-handler');
 const UserModel = require('../models/userModel');
-
+const { query } = require('express');
+const EventModel = require('../models/eventModel');
 
 const getAllUsers = asyncHandle(async (req, res) => {
     const users = await UserModel.find({});
@@ -22,4 +24,26 @@ const getAllUsers = asyncHandle(async (req, res) => {
     });
 });
 
-module.exports = { getAllUsers };
+const getEventsFollowed = asyncHandle(async (req, res) => {
+    const { uid } = req.query;
+
+    if (uid) {
+        console.log(uid);
+        const events = await EventModel.find({ followers: { $all: uid } });
+
+
+        const ids = [];
+
+        events.forEach((event) => ids.push(event.id));
+
+        res.status(200).json({
+            message: 'Get follower successfully',
+            data: ids,
+        });
+    } else {
+        res.sendStatus(401);
+        throw new Error('Missing uid');
+    }
+});
+
+module.exports = { getAllUsers, getEventsFollowed };
