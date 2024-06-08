18d826eff51cbc64f6a11ad4a76706ca79c91d53
Le Tien <tienle21032001@gmail.com>
2024-06-08 17:57:52 +0700

    get profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,12 @@
 
 const Router = require('express');
-const { getAllUsers, getEventsFollowed, updateFcmToken } = require('../controllers/userController');
+const { getAllUsers, getEventsFollowed, updateFcmToken, getProfile } = require('../controllers/userController');
 
 const userRouter = Router();
 
 userRouter.get('/get-all', getAllUsers);
 userRouter.get('/get-followed-events', getEventsFollowed);
 userRouter.post('/update-fcmtoken', updateFcmToken);
-// userRouter.get('/get-profile', getProfile);
+userRouter.get('/get-profile', getProfile);
 
 module.exports = userRouter;
